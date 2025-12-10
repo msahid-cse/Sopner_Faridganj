042ed50550ab5e2f8099a1e420e6175693344505
Md. Sahid <msahid.cse@gmail.com>
2025-12-10 19:30:49 +0600

    feat: Add new madrasha and Faridganj Thana data, and update info desk display.

---
+++ faridganj_thana.js
@@ -0,0 +1,27 @@
+const policeData = {
+    officials: [
+        {
+            image: "img.png",
+            name: "জনাব মোহাম্মদ শাহ্ আলম পিপিএম-সেবা",
+            designation: "অফিসার ইনচার্জ, ফরিদগঞ্জ থানা, চাঁদপুর",
+            email: "oc.faridganjps@gmail.com",
+            mobile: "০১৩২০-১১৬১২৭",
+            officePhone: "০৮৪২২৬৪০১২"
+        },
+        {
+            image: "img.png",
+            name: "জনাব রাজীব চক্রবর্তী",
+            designation: "পুলিশ পরিদর্শক (তদন্ত), ফরিদগঞ্জ থানা, চাঁদপুর",
+            email: "occha.far@police.gov.bd",
+            mobile: "০১৩২০-১১৬১২৮",
+            officePhone: "০১৩২০-১১৬১২৮"
+        }
+    ],
+    staff: [
+        { image: "img.png", name: "মোহাম্মদ আলী ভূঁইয়া", designation: "এএসআই (নিরস্ত্র)", mobile: "০১৭৬০৮৮৬৬৬১" },
+        { image: "img.png", name: "মোঃ ইসমাইল হোসেন", designation: "এসআই (নিরস্ত্র)", mobile: "০১৮১৮-৮২৬৮৪৯" },
+        { image: "img.png", name: "খোকন চন্দ্র দাশ", designation: "এসআই (নিরস্ত্র)", mobile: "০১৮৪০-৮৮০৯৮৮" },
+        { image: "img.png", name: "মোঃ একরামুল হক", designation: "এসআই (নিরস্ত্র)", mobile: "০১৮১৬-১৫৪৬৭২" },
+        { image: "img.png", name: "মোঃ মাহবুবুর ইসলাম", designation: "এসআই (নিরস্ত্র)", mobile: "০১৮৯২-৭৭৯৯৪০" }
+    ]
+};
