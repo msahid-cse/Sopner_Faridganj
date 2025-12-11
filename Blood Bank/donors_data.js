d477266765d6d76f1d6a0807d975f1720f11fe87
Md. Sahid <msahid.cse@gmail.com>
2025-12-11 19:19:32 +0600

    feat: Implement blood bank UI and donor data management.

---
+++ Blood Bank/donors_data.js
@@ -0,0 +1,29 @@
+// Blood Donor Database
+// স্বপ্নের ফরিদগঞ্জ - Blood Bank System
+
+const donors = [
+    {
+        id: 101,
+        name: "Md. Sahid",
+        group: "B+",
+        zila: "Chandpur",
+        upazila: "ফরিদগঞ্জ",
+        phone: "01407531529",
+        lastDonation: "নতুন দাতা",
+        contactMethods: "Whatsapp, Imo, Call, Message",
+        facebookUrl: "https://www.facebook.com/msahid.cse"
+    }
+    // Add more donors here
+    // Example:
+    // {
+    //     id: 102,
+    //     name: "Donor Name",
+    //     group: "A+",
+    //     zila: "Dhaka",
+    //     upazila: "ধানমন্ডি",
+    //     phone: "01XXXXXXXXX",
+    //     lastDonation: "১ মাস আগে",
+    //     contactMethods: "Whatsapp, Call",
+    //     facebookUrl: "https://www.facebook.com/username"
+    // }
+];
